2e01e73147c063e55289013d31ca570a77ed40a4
alan <daowdalan@gmail.com>
2023-08-04 18:29:32 +0300

    4

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from .models import Rating, Meal
+from django.contrib.auth.models import User
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
 class MealSerializer(serializers.ModelSerializer):
     class Meta:
